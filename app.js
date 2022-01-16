31e9b06f1436cd6972560243723c2169138dfc43
Tamtama <tamtamaXandynata@gmail.com>
2022-01-16 15:27:08 +0700

    berhasil get data posts

--- app.js
+++ app.js
@@ -8,11 +8,30 @@ app.use(express.urlencoded({
     extended:true
 }))
 
+const db = require('./app/models')
+
+db.mongoose
+.connect(db.url,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=>{
+    console.log('database connected!');
+}).catch((err)=>{
+    console.log('cannot connect to the database'+ err);
+    process.exit();
+});
+
+
+
+
+
 app.get('/',(req,res)=>{
     res.json({
         message:'welcome to express tutorial'
     })
 })
+
+require('./app/routes/post.routes')(app)
 
 const PORT = 8000
 
---
+++ app/routes/post.routes.js
@@ -0,0 +1,10 @@
+
+
+module.exports =(app) =>{
+    const posts = require('../controllers/post.controller')
+    const router = require('express').Router()
+
+    router.get('/', posts.findAll)
+
+    app.use('/api/posts', router)
+}
