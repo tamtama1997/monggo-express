d8a848f891615a5b8157a03760973a1097a7d459
Tamtama <tamtamaXandynata@gmail.com>
2022-01-18 14:35:57 +0700

    make it better

--- config/db.config.js
+++ config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb+srv://tamtama1997:1234321@clustertama.b2yzv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+    url: "mongodb://base-be:test123@cluster0-shard-00-00.m8ych.mongodb.net:27017,cluster0-shard-00-01.m8ych.mongodb.net:27017,cluster0-shard-00-02.m8ych.mongodb.net:27017/test?replicaSet=atlas-4czntg-shard-0&ssl=true&authSource=admin"
 }
